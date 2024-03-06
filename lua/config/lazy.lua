864de593626d2b46509ca77a7ddeb9d15bcb2ea0
iamky <yadav.kanishk01@gmail.com>
2024-03-06 21:38:36 +0530

    More tweaks

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -9,12 +9,14 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = { colorscheme = "ayu-dark"} },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = { colorscheme = "ayu-dark" } },
     -- import any extras modules here
-       { import = "lazyvim.plugins.extras.linting.eslint" },
-       { import = "lazyvim.plugins.extras.formatting.prettier" },
-    -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
